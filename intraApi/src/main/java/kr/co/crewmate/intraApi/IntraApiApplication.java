979f9ea00b2c7e045b57f5fe47e0e17bc8595270
kskim <kskim@crewmate.co.kr>
2019-09-26 10:17:34 +0900

    git test

---
+++ intraApi/src/main/java/kr/co/crewmate/intraApi/IntraApiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.crewmate.intraApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntraApiApplication.class, args);
+	}
+
+}
---
+++ intraApi/src/test/java/kr/co/crewmate/intraApi/IntraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.crewmate.intraApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
